38a582979218e3c0e1cb6c2dd25697da631fe644
Mike Mitterer <office@mikemitterer.at>
2019-10-15 15:59:39 +0200

    Inline-Edit funkt für die Description

--- src/components/ArticleComponent.vue
+++ src/components/ArticleComponent.vue
@@ -1,40 +1,81 @@
 <template>
-    <v-card :class="{ 'light-green lighten-3': addMode }" class="mx-auto mb-3" max-width="350">
-        <v-card-text>
-            <div class="font-weight-light">{{ article.id }}</div>
-            <p class="display-1 text--primary" v-if="!addMode">
-                {{ article.description }}
-            </p>
-            <v-text-field
-                v-model="article.description"
-                label="Description*"
-                required
-                v-if="addMode"
-            ></v-text-field>
-            <div>
-                <div class="font-weight-bold">{{ article.price }}</div>
-            </div>
-        </v-card-text>
-        <!-- class="justify-end" -->
-        <v-card-actions>
-            <v-btn v-if="addMode" text color="deep-purple accent-4" @click="$emit('cancel')">
-                Cancel
-            </v-btn>
-            <v-spacer></v-spacer>
-            <!-- router-link mit 'to' würde auch gehen -->
-            <v-btn v-if="!addMode" text color="deep-purple accent-4" :to="`/article/${article.id}`">
-                More...
-            </v-btn>
-            <v-btn v-if="addMode" color="deep-purple accent-4 primary" @click="$emit('save-item')">
-                Save
-            </v-btn>
-        </v-card-actions>
-    </v-card>
+    <v-hover v-slot:default="{ hover }">
+        <v-card
+            :class="{ 'light-green lighten-3': addMode }"
+            class="mx-auto mb-3"
+            max-width="350"
+            :elevation="hover ? 12 : 2"
+        >
+            <v-card-title class="purple font-weight-light subtitle-1">
+                <div>{{ article.id }}</div>
+            </v-card-title>
+
+            <v-card-text>
+                <p
+                    class="headline text--primary"
+                    v-if="!addMode && !editHeadline"
+                    @click="onEditDescription"
+                >
+                    {{ article.description }}
+                </p>
+                <v-row v-if="editHeadline && isActive">
+                    <v-col cols="12" class="py-0">
+                        <v-text-field
+                            v-model="tempArticle.description"
+                            label="Description*"
+                            required
+                            autofocus
+                            @keyup.esc="onCancel"
+                            @keyup.enter="onSave"
+                        ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12">
+                        <v-btn small class="mx-2" @click="onCancel">Cancel</v-btn>
+                        <v-btn small color="primary" class="mx-2" @click="onSave">Save</v-btn>
+                    </v-col>
+                </v-row>
+                <div>
+                    <div class="font-weight-bold">{{ article.price }}</div>
+                </div>
+            </v-card-text>
+
+            <!-- class="justify-end" -->
+            <v-card-actions>
+                <v-btn v-if="addMode" text color="deep-purple accent-4" @click="$emit('cancel')">
+                    Cancel
+                </v-btn>
+                <!-- router-link mit 'to' würde auch gehen -->
+                <v-btn
+                    v-if="!addMode"
+                    text
+                    color="deep-purple accent-4"
+                    :to="`/article/${article.id}`"
+                >
+                    More...
+                </v-btn>
+                <v-btn
+                    v-if="addMode"
+                    color="deep-purple accent-4 primary"
+                    @click="$emit('save-item')"
+                >
+                    Save
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-icon
+                    @click="$emit('delete-item', article.id)"
+                    class="trash"
+                    :class="{ 'on-hover': hover }"
+                    >mdi-trash-can
+                </v-icon>
+            </v-card-actions>
+        </v-card>
+    </v-hover>
 </template>
 
 <script lang="ts">
 import { Article } from '@/model/Article';
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
 @Component
 export default class ArticleComponent extends Vue {
@@ -43,7 +84,52 @@ export default class ArticleComponent extends Vue {
 
     @Prop({ default: false })
     private addMode!: boolean;
+
+    @Prop({ default: false })
+    private isActive!: boolean;
+
+    private editHeadline = false;
+
+    private tempArticle: Article | '' = '';
+
+    @Watch('isActive')
+    private onIsActiveChanged(val: boolean, oldVal: boolean): void {
+        if (!val) {
+            this.editHeadline = false;
+        }
+    }
+
+    // noinspection JSUnusedLocalSymbols
+    private onEditDescription(): void {
+        this.tempArticle = Object.assign(this.tempArticle, this.article);
+        this.editHeadline = true;
+
+        this.$emit('activate');
+    }
+
+    // noinspection JSUnusedLocalSymbols
+    private onCancel(): void {
+        this.tempArticle = '';
+        this.editHeadline = false;
+        this.$emit('activate', false);
+    }
+
+    // noinspection JSUnusedLocalSymbols
+    private onSave(): void {
+        this.article = Object.assign(this.article, this.tempArticle);
+        this.tempArticle = '';
+        this.editHeadline = false;
+        this.$emit('activate', false);
+    }
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.trash {
+    visibility: hidden;
+
+    &.on-hover {
+        visibility: visible;
+    }
+}
+</style>
--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -3,7 +3,7 @@
         <v-btn fixed dark fab bottom right color="pink" class="mb-10" @click="addItem">
             <v-icon>mdi-plus</v-icon>
         </v-btn>
-        <h1 class="display-2">Articles</h1>
+        <h1 class="display-2 mb-3">Articles</h1>
         <ArticleComponent
             v-if="isInAddMode"
             :article="item"
@@ -11,13 +11,23 @@
             @save-item="onSaveItem"
             @cancel="onCancel"
         ></ArticleComponent>
-        <div class="d-flex flex-wrap justify-space-around">
-            <ArticleComponent
-                class="ml-2"
-                :article="article"
-                v-for="article in articles"
-                :key="article.id"
-            ></ArticleComponent>
+        <div>
+            <v-item-group class="grid-container">
+                <v-item
+                    v-for="article in articles"
+                    :key="article.id"
+                    v-slot:default="{ active, toggle }"
+                >
+                    <ArticleComponent
+                        class="ml-2"
+                        :article="article"
+                        @delete-item="onDelete"
+                        @activate="toggle"
+                        :class="{ 'light-green lighten-3': active }"
+                        :isActive="active"
+                    ></ArticleComponent>
+                </v-item>
+            </v-item-group>
         </div>
     </v-container>
 </template>
@@ -62,8 +72,38 @@ export default class Articles extends Vue {
 
     // noinspection JSUnusedLocalSymbols
     private onCancel(): void {
+        this.logger.info('Cancel');
         this.item = undefined;
+    }
+
+    // noinspection JSUnusedLocalSymbols
+    private onDelete(id: string): void {
+        this.logger.info(`Delete ITEM: ${id}`);
+        crudModule.delete(id);
+    }
+
+    // noinspection JSUnusedLocalSymbols
+    private onTestClick(id: string): void {
+        this.logger.info(`Test-Click`);
     }
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.grid-container {
+    /*display: grid;*/
+    /*grid-template-columns: 1fr 1fr 1fr;*/
+    /*grid-template-rows: 1fr 200px;*/
+    /*grid-column-gap: 20px;*/
+    /*grid-row-gap: 20px;*/
+    /*justify-items: stretch;*/
+    /*align-items: start;*/
+
+    display: grid;
+    grid-gap: 10px;
+    // grid-template-columns: repeat(auto-fit, 300px);
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+
+    justify-items: stretch;
+    align-items: start;
+}
+</style>
