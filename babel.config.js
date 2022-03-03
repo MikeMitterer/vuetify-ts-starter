302330caf3c8db49d836c5dec3c4efdbf9c46469
Mike Mitterer <office@mikemitterer.at>
2022-03-03 14:36:23 +0100

    Compiler-Probleme nach upgrade

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,28 @@
 module.exports = {
-    "presets": [
+    presets: [
         [
             // Mehr: https://babeljs.io/docs/en/babel-preset-env
-            "@babel/preset-env",
+            '@babel/preset-env',
             {
-                "targets": {
+                targets: {
                     // When specifying this option, the browsers field will be ignored.
                     // "esmodules": true,
 
-                    "node": "current",
-                    "browsers": [
-                        "last 2 Chrome versions",
-                        "last 1 Safari versions",
-                        "last 1 Firefox versions"
+                    node: 'current',
+                    browsers: [
+                        'last 2 Chrome versions',
+                        'last 1 Safari versions',
+                        'last 1 Firefox versions'
                     ]
-
                 },
                 // Enable transformation of ES6 module syntax to another module type.
                 // Setting this to false will not transform modules.
                 //
                 // Muss auf "auto" stehen sonst funktioniert JEST nicht!
-                "modules": "auto"
+                modules: 'auto'
             }
         ],
-        "@babel/typescript"
+        '@babel/typescript'
     ],
     // "env": {
     //     // BABEL_ENV=test <command (e.g. yarn test:unit)>
@@ -31,4 +30,12 @@ module.exports = {
     //         "plugins": ["@babel/plugin-transform-modules-commonjs"]
     //     }
     // },
-};
+    plugins: [
+        // yarn add -D "@babel/plugin-proposal-class-properties" "@babel/plugin-proposal-object-rest-spread" "@babel/plugin-transform-runtime" "@babel/plugin-proposal-object-rest-spread" "@babel/plugin-proposal-nullish-coalescing-operator" "@babel/plugin-proposal-optional-chaining" "babel-plugin-transform-inline-environment-variables"
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-transform-runtime'
+    ]
+}
